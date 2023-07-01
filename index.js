218ad3775d4a7921de53d54e0f19aee0e40f97e9
Tien <45297291+tienwong@users.noreply.github.com>
2023-06-30 22:49:39 -0500

    Made sure MongoDB connects properly

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI
 
 app.use(cors());
 
@@ -17,9 +19,16 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/shorturl', urlEncodedParser, (req, res) => {
-  // dummy implementation just to make sure this is working right
-  res.send({ original_url: req.body.url, short_url: 'for right now this is just something random!' })
+app.post('/api/shorturl', urlEncodedParser, async (req, res) => {
+  try {
+    const con = await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true})
+    console.log('MongoDB is connected.')
+    // dummy implementation just to make sure this is working right
+    res.send({ original_url: req.body.url, short_url: 'for right now this is just something random!' })
+  }
+  catch (err) {
+    res.send({ error: err })
+  }
 })
 
 // Your first API endpoint
